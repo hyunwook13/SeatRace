1082f7e20d159dd9d1ac9547236e002ce77d432c
Hyunwook <hjhsha827@gmail.com>
2026-03-06 19:55:18 +0900

    init: project start

---
+++ src/test/java/org/example/seatrace/SeatRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seatrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatRaceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
