08756ad2adcab01198320c3fd9802200f8e89eb1
Hyunwook <hjhsha827@gmail.com>
2026-03-17 14:59:48 +0900

    feat: security 추가 및 CustomUserDetailsService 완성

--- src/main/java/org/example/seatrace/user/CustomUserDetailsService.java
+++ src/main/java/org/example/seatrace/user/CustomUserDetailsService.java
@@ -1,5 +1,28 @@
 package org.example.seatrace.user;
 
+import lombok.RequiredArgsConstructor;
+import org.example.seatrace.entity.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.List;
+
+
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+    return new org.springframework.security.core.userdetails.User(
+        user.getEmail(),
+        user.getPasswordHash(),
+        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+    );
+  }
 }
--- src/main/java/org/example/seatrace/user/UserRepository.java
+++ src/main/java/org/example/seatrace/user/UserRepository.java
@@ -5,5 +5,5 @@ import org.example.seatrace.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByUsername(String username);
+  Optional<User> findByEmail(String email);
 }
