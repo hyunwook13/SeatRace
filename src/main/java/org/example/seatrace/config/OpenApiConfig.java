f6c5e30b92f7a784fd5e6cd4aee0791724dc82b4
Hyunwook <hjhsha827@gmail.com>
2026-03-19 16:14:19 +0900

    feat: swagger 설정 추가

---
+++ src/main/java/org/example/seatrace/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package org.example.seatrace.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "SeatRace API",
+        version = "v1",
+        description = "좌석 예약 서비스 API"
+    )
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
