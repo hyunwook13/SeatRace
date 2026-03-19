98a0cbdaa2bceed26b801bc040daea7388187e6c
Hyunwook <hjhsha827@gmail.com>
2026-03-19 16:05:13 +0900

    feat: objectmapper 없는 문제 해결

--- src/main/java/org/example/seatrace/config/AppConfig.java
+++ src/main/java/org/example/seatrace/config/AppConfig.java
@@ -1,4 +1,18 @@
 package org.example.seatrace.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableJpaAuditing
 public class AppConfig {
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
+  }
 }
