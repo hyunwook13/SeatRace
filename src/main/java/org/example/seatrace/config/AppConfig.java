ea2dc76119da9fc434a0d9bc9f8333d3ce8fe70f
Hyunwook <hjhsha827@gmail.com>
2026-03-26 01:01:51 +0900

    feat: framework + 기타 추가

--- src/main/java/org/example/seatrace/config/AppConfig.java
+++ src/main/java/org/example/seatrace/config/AppConfig.java
@@ -1,12 +1,14 @@
 package org.example.seatrace.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
+@ConfigurationPropertiesScan
 @EnableScheduling
 @EnableJpaAuditing
 public class AppConfig {
