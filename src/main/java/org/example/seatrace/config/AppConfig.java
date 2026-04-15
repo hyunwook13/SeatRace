fba4e3d4178b2e49171658c6097fd42519ae902d
Hyunwook <hjhsha827@gmail.com>
2026-04-15 17:34:46 +0900

    chore: align spring boot dependencies

--- src/main/java/org/example/seatrace/config/AppConfig.java
+++ src/main/java/org/example/seatrace/config/AppConfig.java
@@ -1,8 +1,6 @@
 package org.example.seatrace.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,9 +10,4 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJpaAuditing
 public class AppConfig {
-
-  @Bean
-  public ObjectMapper objectMapper() {
-    return new ObjectMapper();
-  }
 }
